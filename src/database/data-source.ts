2259bd9e590a1d72153bbc663cdf348d930e41b9
Phat Bien <phat.bien@manifera.com>
2026-06-18 15:14:48 +0700

    typeorm script

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
-import { DataSource, DataSourceOptions } from 'typeorm';
+import { DataSource } from 'typeorm';
+import type { DataSourceOptions } from 'typeorm';
 
 export const AppDataSource = new DataSource({
   type: process.env.DATABASE_TYPE,
